cf773d6e72264e8284ed141dada7ef94acba6cf3
David Ferreira Lima <dfl8@aluno.ifal.edu.br>
2024-11-29 16:25:02 -0300

    feat: add UIImageView extension

--- Swither/Swither/Extensions/UIImageView+Extensions.swift
+++ Swither/Swither/Extensions/UIImageView+Extensions.swift
@@ -7,34 +7,16 @@
 
 import UIKit
 
-extension UIImage {
-    static func from(url urlString: String, completion: @escaping (UIImage?) -> Void) {
-        guard let url = URL(string: urlString) else {
-            print("Invalid URL string")
-            completion(nil)
-            return
-        }
-        
-        // Fetch image data asynchronously
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            // Handle any error that occurred during fetching
-            if let error = error {
-                print("Error fetching image: \(error)")
-                completion(nil)
-                return
-            }
-            
-            // If data is received, create the image
-            if let data = data, let image = UIImage(data: data) {
-                // Ensure the completion handler is called on the main thread
-                DispatchQueue.main.async {
-                    completion(image)
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
                 }
-                return
-            } else {
-                completion(nil)
-                return
             }
-        }.resume() // Start the task
+        }
     }
 }
